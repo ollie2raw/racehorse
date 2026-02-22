b2114f1069d1a50eadf3ab050ef6fc3134d06a22
oliver morid <olivermorid@Mac.attlocal.net>
2026-02-22 01:20:10 -0800

    remove premium-polish css conflict, restore walnut-live layout

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import './styles/premium-polish.css'
 import './styles/walnut-live.css'
 
 createRoot(document.getElementById('root')!).render(
