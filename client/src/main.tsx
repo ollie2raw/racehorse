549b96caa3ccaa13ebfd5b990263e4735a5a37ac
oliver morid <olivermorid@Mac.attlocal.net>
2026-02-22 20:59:09 -0800

    chore(client): install global error handlers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { installGlobalErrorHandlers } from "./debug/globalErrors";
 import './index.css';
 import App from './App.tsx';
 import './styles/walnut-live.css';
+
+{installGlobalErrorHandlers();}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
