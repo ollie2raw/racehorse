7e72ffb2fe99b1e95dad0469be372817f52e0c63
Oliver Morid <moridoliver@gmail.com>
2026-04-01 21:28:27 -0700

    newUI

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { installGlobalErrorHandlers } from "./debug/globalErrors";
 import './index.css';
+import './premium-theme.css';
 import App from './App.tsx';
 import './styles/walnut-live.css';
 
